87cd0bae5e3646174de5c0817dbe6be627274a3a
Noppawit Jomkittichai <noppawit.test@mail.kmutt.ac.th>
2022-12-09 16:42:59 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static string[] supportsymbol = new string[]{'(', ')'};
+
+    static void Inputsymbol(Queue<string> charactor)
+    {
+        string symbol = (Console.ReadLine());
+        if(supportsymbolArray.Contains(symbol))
+        {
+            charactor.Push(symbol);
+            Inputsymbol(charactor);
+        }
+    }
+    
+   
+    static void Processcharactor(Queue<string> charactor)
+    {
+        string symbol;
+        while(charactor.GetLength() > 0)
+        {
+            symbol = charactor.Pop();
+            switch(symbol)
+            {
+                case '(':
+                    Console.Write(valid);
+                    break;
+                case ')':
+                    Console.Write(valid);
+                    break;
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Queue<string> charactor = new Queue<string>();
+        Inputsymbol(charactor);
+        Processcharactor(charactor);
+    }
+}
